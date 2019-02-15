7a220aeb70e4b184aa98371c5b57b36f8dbb2255
Dusan <masinacdt@gmail.com>
2019-02-15 13:55:20 +0100

    singleton 2 added

---
+++ singleton2.php
@@ -0,0 +1,30 @@
+<?php
+
+class Singleton
+{
+    private static $instance = null;
+
+    private function __construct()
+    {
+
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new Singleton();
+        }
+
+        return self::$instance;
+    }
+
+}
+
+//$objekat = new Singleton();
+//Fatal error: Uncaught Error: Call to private Singleton::__construct() from
+//invalid context
+
+$objekat= Singleton::getInstance();
+var_dump($objekat);
+
+ ?>
