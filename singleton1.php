38cb1745366c8068b9f87af933f964209e8db292
Dusan <masinacdt@gmail.com>
2019-02-15 13:47:46 +0100

    singleton working

---
+++ singleton1.php
@@ -0,0 +1,45 @@
+<?php
+class Singleton
+{
+    public static function getInstance()
+    {
+        static $instance = null;
+
+        if($instance === null) {
+            $instance = new static();
+        }
+
+    return $instance;
+    }
+
+    protected function __construct()
+    {
+
+    }
+
+    private function __clone()
+    {
+
+    }
+
+    private function __wakeup()
+    {
+
+    }
+
+}
+
+class SingletonDete extends Singleton
+{
+
+}
+
+$obj = Singleton::getInstance();
+var_dump($obj === Singleton::getInstance());
+
+$another_obj = SingletonDete::getInstance();
+var_dump($another_obj === Singleton::getInstance());
+var_dump($another_obj === SingletonDete::getInstance());
+
+
+ ?>
